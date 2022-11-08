46329a424d768a04cfa907a8559799ff697d761a
mrtkarakis <mertkaraks@icloud.com>
2022-11-08 19:27:34 +0300

    some changes

--- lib/core/widgets/phone.dart
+++ lib/core/widgets/phone.dart
@@ -22,38 +22,38 @@ class PhoneCase extends StatelessWidget {
     return LayoutBuilder(
       // If our width is more than 1100 then we consider it a desktop
       builder: (context, constraints) {
-        if (isFull(context)) {
-          return Container(
-              width: width,
-              height: 750,
-              padding: EdgeInsets.symmetric(vertical: 65, horizontal: 30),
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/icons/phone_case.png'),
-                ),
+        // if (isFull(context)) {
+        return Container(
+            width: width,
+            height: 750,
+            padding: EdgeInsets.symmetric(vertical: 65, horizontal: 30),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/icons/phone_case.png'),
               ),
-              child: GridView.builder(
-                  itemCount: AppType.values.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4),
-                  itemBuilder: (BuildContext context, int index) {
-                    return linkButton(AppType.values[index]);
-                  })
-              //  Column(
-              //   children: [
-              //     Row(
-              //       children: [
-              //         linkButton(LinkType.github),
-              //         linkButton(LinkType.medium),
-              //         linkButton(LinkType.linkedin),
-              //         linkButton(LinkType.github),
-              //       ],
-              //     )
-              //   ],
-              // ),
-              );
-        } else
-          return Container();
+            ),
+            child: GridView.builder(
+                itemCount: AppType.values.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  return linkButton(AppType.values[index]);
+                })
+            //  Column(
+            //   children: [
+            //     Row(
+            //       children: [
+            //         linkButton(LinkType.github),
+            //         linkButton(LinkType.medium),
+            //         linkButton(LinkType.linkedin),
+            //         linkButton(LinkType.github),
+            //       ],
+            //     )
+            //   ],
+            // ),
+            );
+        // } else
+        //   return Container();
       },
     );
   }
@@ -120,17 +120,17 @@ enum AppType {
   linkedin('LinkedIn', 'https://www.linkedin.com/in/mert-karakis/', 'linkedin',
       false),
   medium('Medium', 'https://medium.com/@mrtkarakis', 'medium', false),
-  mail('Mail', 'https://medium.com/@mrtkarakis', 'mail', false);
-  // contacts('Contacts', 'https://medium.com/@mrtkarakis', 'contacts', true),
-  // aboutMe('About Me', 'https://medium.com/@mrtkarakis', 'about_me', true),
-  // technologies(
-  // 'Technologies', 'https://medium.com/@mrtkarakis', 'technologies', true),
-  // tokenGo('Token Go', 'https://medium.com/@mrtkarakis', 'token_go_logo', true),
-  // cmonHealth('Cmon Health', 'https://medium.com/@mrtkarakis',
-  //     'cmon_health_logo', true),
-  // cryptory('Cryptory', 'https://medium.com/@mrtkarakis', 'cryptory_logo', true),
-  // slothy('Slothy', 'https://medium.com/@mrtkarakis', 'slothy_logo', true),
-  // requestJob('Request Job', 'https://medium.com/@mrtkarakis', 'add', true);
+  mail('Mail', 'https://medium.com/@mrtkarakis', 'mail', false),
+  contacts('Contacts', 'https://medium.com/@mrtkarakis', 'contacts', true),
+  aboutMe('About Me', 'https://medium.com/@mrtkarakis', 'about_me', true),
+  technologies(
+      'Technologies', 'https://medium.com/@mrtkarakis', 'technologies', true),
+  tokenGo('Token Go', 'https://medium.com/@mrtkarakis', 'token_go_logo', true),
+  cmonHealth('Cmon Health', 'https://medium.com/@mrtkarakis',
+      'cmon_health_logo', true),
+  cryptory('Cryptory', 'https://medium.com/@mrtkarakis', 'cryptory_logo', true),
+  slothy('Slothy', 'https://medium.com/@mrtkarakis', 'slothy_logo', true),
+  requestJob('Request Job', 'https://medium.com/@mrtkarakis', 'add', true);
 
   final String title;
   final String url;
--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -30,69 +30,70 @@ class _MainPageState extends State<MainPage> {
     List<AppType> links = AppType.values.toList();
 
     return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
-        width: size.width,
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Spacer(),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(99),
-                child: Image.asset(
-                  "assets/images/profile_photo.png",
-                  height: 99,
-                ),
-              ),
-              const Text("Mert Karakış"),
-              const Spacer(),
-              SizedBox(
-                height: 75,
-                width: (80 * links.length).toDouble(),
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: AppType.values.length,
-                  itemExtent: 80,
-                  itemBuilder: (BuildContext context, int index) {
-                    final AppType type = AppType.values[index];
-                    final String iconPath = type.fullPath();
-                    final String name = type.title;
-                    final String url = type.url;
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+      width: size.width,
+      child:
+          //  Container(
+          //   child: Column(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: [
+          //       const Spacer(),
+          //       ClipRRect(
+          //         borderRadius: BorderRadius.circular(99),
+          //         child: Image.asset(
+          //           "assets/images/profile_photo.png",
+          //           height: 99,
+          //         ),
+          //       ),
+          //       const Text("Mert Karakış"),
+          //       const Spacer(),
+          //       SizedBox(
+          //         height: 75,
+          //         width: (80 * links.length).toDouble(),
+          //         child: ListView.builder(
+          //           scrollDirection: Axis.horizontal,
+          //           itemCount: AppType.values.length,
+          //           itemExtent: 80,
+          //           itemBuilder: (BuildContext context, int index) {
+          //             final AppType type = AppType.values[index];
+          //             final String iconPath = type.fullPath();
+          //             final String name = type.title;
+          //             final String url = type.url;
 
-                    return TextButton(
-                      onPressed: () {
-                        type.title.toLowerCase() == 'mail'
-                            ? launchUrl(Uri(
-                                scheme: 'mailto',
-                                path: 'mekonyum1@gmail.com',
-                                query:
-                                    'subject=App Feedback&body=App Version 3.23', //add subject and body here
-                              ))
-                            : launchUrl(
-                                Uri.parse(
-                                  url,
-                                ),
-                                mode: LaunchMode.platformDefault,
-                              );
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Image.asset(iconPath, height: 50),
-                          Text(name),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const Spacer(),
-            ],
-          ),
-        )
-        //  Row(
-        //   children: const [PhoneCase(), SizedBox(width: 20), AppPage()],
-        // ),
-        );
+          //             return TextButton(
+          //               onPressed: () {
+          //                 type.title.toLowerCase() == 'mail'
+          //                     ? launchUrl(Uri(
+          //                         scheme: 'mailto',
+          //                         path: 'mrtkarakis@gmail.com',
+          //                         query:
+          //                             'subject=App Feedback&body=App Version 3.23', //add subject and body here
+          //                       ))
+          //                     : launchUrl(
+          //                         Uri.parse(
+          //                           url,
+          //                         ),
+          //                         mode: LaunchMode.platformDefault,
+          //                       );
+          //               },
+          //               child: Column(
+          //                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //                 children: [
+          //                   Image.asset(iconPath, height: 50),
+          //                   Text(name),
+          //                 ],
+          //               ),
+          //             );
+          //           },
+          //         ),
+          //       ),
+          //       const Spacer(),
+          //     ],
+          //   ),
+          // )
+          Row(
+        children: const [PhoneCase(), SizedBox(width: 20), AppPage()],
+      ),
+    );
   }
 }
